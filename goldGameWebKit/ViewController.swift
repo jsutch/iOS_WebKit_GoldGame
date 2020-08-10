3e1f392f327d56bb2c7fd68a182876dd0bdecd26
Jeff Sutch <jeff.sutch@acm.org>
2020-08-10 12:47:16 -0700

    Initial Commit

---
+++ goldGameWebKit/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  goldGameWebKit
+//
+//  Created by Jeff Sutch on 8/10/20.
+//  Copyright © 2020 Collett Park Networks. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let webView = WKWebView(frame: view.frame)
+        view.addSubview(webView)
+        
+        let url = URL(string: "https://www.suckerpun.ch/goldgame")!
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+
+
+}
+
